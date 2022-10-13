32f2da44b0cec04e989d2dabcec60f806bf802fc
ms <soe3676@nate.com>
2022-10-13 15:18:14 +0900

    JDBC 연결

---
+++ testDBEx1/src/execute.java
@@ -0,0 +1,11 @@
+public class execute {
+    String name;
+    int phone;
+    String email;
+
+    execute(String name, int phone, String email) {
+        this.name = name;
+        this.phone = phone;
+        this.email = email;
+    }
+}
